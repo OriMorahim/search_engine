fe0a70e28dba0ce98ca688ccb6cfde375c5f95dd
OriMorahim <orimorhaim22@gmail.com>
2020-11-21 18:32:51 +0200

    add indexer swap upper case letters

--- parser_module.py
+++ parser_module.py
@@ -1,17 +1,20 @@
 import re
 from document import Document
-from collections import Counter
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+from collections import Counter, defaultdict
 
-
-WORDS_COUNTER = Counter()
 
 class Parse:
 
     def __init__(self):
         self.stop_words = stopwords.words('english')
+        self.dictionary = defaultdict(set)
+        self.tweets_words_locations = dict()
+        self.capitals_counter = Counter()
+        self.words_dual_representation = []
+        self.words_capital_representation = []
 
     def parse_sentence(self, text, do_stem: bool):
         """
@@ -30,7 +33,7 @@ class Parse:
 
         text_tokens_without_stopwords = [w for w in text_tokens if w.lower() not in self.stop_words]
 
-        WORDS_COUNTER.update(text_tokens_without_stopwords)
+        self.capitals_counter.update(text_tokens_without_stopwords)
 
         return text_tokens_without_stopwords
 
@@ -48,17 +51,22 @@ class Parse:
         retweet_url = doc_as_named_tuple.retweet_urls
         quote_text = doc_as_named_tuple.quoted_text
         quote_url = doc_as_named_tuple.quote_urls
-        term_dict = {}
         tokenized_text = self.parse_sentence(full_text, do_stem)
-
+        term_dict = Counter(tokenized_text)
         doc_length = len(tokenized_text)  # after text operations.
 
-        for term in tokenized_text:
-            if term not in term_dict.keys():
-                term_dict[term] = 1
-            else:
-                term_dict[term] += 1
+        # the following dict will hold the words locations for a specific tweet
+        tweet_words_locations = defaultdict(list)
 
+        # add words to dict
+        for location, term in enumerate(tokenized_text):
+                self.dictionary[term].add(tweet_id)
+                tweet_words_locations[term].append(location)
+
+        # keep tweet words locations
+        self.tweets_words_locations[tweet_id] = tweet_words_locations
+
+        # insert data to Documnet structure
         document = Document(tweet_id, tweet_date, full_text, url, retweet_text, retweet_url, quote_text,
                             quote_url, term_dict, doc_length)
         return document
@@ -69,25 +77,38 @@ class Parse:
         :param df:
         :return:
         """
+        # parse each tweet and insert result to a document
         documnets = []
         for row in df.itertuples():
             document = self.parse_doc(row)
             documnets.append(document)
 
         # fetch words with capital letter
-        all_words = set(WORDS_COUNTER.keys())
+        all_words = set(self.capitals_counter.keys())
         uppers_as_lowers = set([word for word in all_words if word[0].isupper()])
-        words_appear_as_upper_and_lower = set([word for word in uppers_as_lowers if word.lower() in all_words])
-        words_appear_as_upper_only = uppers_as_lowers-words_appear_as_upper_and_lower
+        self.words_dual_representation = set([word for word in uppers_as_lowers if word.lower() in all_words])
+        self.words_capital_representation = uppers_as_lowers-self.words_dual_representation
 
         # for each word that appear only with capital change the word to be all capital
         for upper_as_lower in uppers_as_lowers:
-            if upper_as_lower in words_appear_as_upper_only:
-                WORDS_COUNTER[upper_as_lower.upper()] = WORDS_COUNTER.pop(upper_as_lower)
+            if upper_as_lower in self.words_capital_representation:
+                self.capitals_counter[upper_as_lower.upper()] = self.capitals_counter.pop(upper_as_lower)
             # if the word appear as capital and lower convert the capital to lower and fix Counter
             else:
-                WORDS_COUNTER[upper_as_lower.lower()] = WORDS_COUNTER[upper_as_lower.lower()] + \
-                                                        WORDS_COUNTER.pop(upper_as_lower)
+                self.capitals_counter[upper_as_lower.lower()] = self.capitals_counter[upper_as_lower.lower()] + \
+                                                        self.capitals_counter.pop(upper_as_lower)
+
+        # change words to all capital
+        for capital in self.words_capital_representation: # list of words need to be change appearnce to capital
+            for tweet in self.dictionary[capital]: # set of tweets contain the capital
+                self.tweets_words_locations[tweet][capital.upper()].extend(self.tweets_words_locations[tweet].pop(capital))
+            self.dictionary[capital.upper()] = self.dictionary.pop(capital)
+
+        # change words to all lower
+        for capital in self.words_dual_representation:
+            for tweet in self.dictionary[capital]:
+                self.tweets_words_locations[tweet][capital.lower()].extend(self.tweets_words_locations[tweet].pop(capital))
+            self.dictionary[capital.lower()].union(self.dictionary.pop(capital))
 
         return documnets
 
@@ -128,3 +149,12 @@ class Parse:
         for word in tokens:
                 dfs.append(ps.stem(word))
         return dfs
+
+
+
+# from parser_module import Parse
+# parse_ = Parse()
+# from reader import ReadFile
+# reader_ = ReadFile('C:/Users/orimo/Documents/study_bgu/information_retrival/Data')
+# df = reader_.read_and_concat_all_parquet_in_dir_of_dirs(1)
+# docs = parse_.parse_corpus(df.head(100))
